0082df954018afd7e34b7711c7b3a9c5087e0e6d
jo-hannsen <johannes.raps@gmx.de>
2023-01-04 00:01:09 +0100

    Update

--- src/components/WebSite/WebSite.js
+++ src/components/WebSite/WebSite.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WebSite = () => {
     return (
         <div className='justfiy-center rounded shadow-md bg-orange border-solid border-2 border-gray-600 w-2/5 py-2 hover:cursor-pointer'>
-            <a className='text-slate-50 font-semibold text-lg' href='https://www.google.com/'>Visit Website</a>
+            <a className='text-slate-50 font-semibold text-lg' href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'>Visit Website</a>
         </div>
     );
 }
